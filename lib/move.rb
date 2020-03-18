8b2c5753d8a305e8cee20994ecf8b8878125dc3f
Ayaz Araf <ayaz.araf@gmail.com>
2020-03-18 02:42:35 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -14,9 +14,6 @@ def move(board,index,character="X")
   return board[index]=character
 end
 
-board = [" ", " ", " "]
-def update_array_at_with(array, index, value)
-  array[index] = value
-end
 
-update_array_at_with(board, 0, "X")
+
+
